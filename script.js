b4f434f9b88d8a0fa008296e2ebb39c714118f00
B-MNSN <manutsanan0@gmail.com>
2022-05-18 15:30:36 +0700

    alert

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-var newText = 'Beer'
-document.getElementById('no').innerHTML = '<b><i>' + newText + '</i></b>'
+// document.getElementById('ok').addEventListener('click', function(e) {
+//     alert('ok is clicked')
+// })
+// document.getElementById('cancel').addEventListener('click', function(e) {
+//     alert('cancel is clicked')
+// })
+// document.getElementById('no').addEventListener('click', function(e) {
+//     alert('no is clicked')
+// })
+
+let elems = document.getElementsByClassName('flex-item')
+console.log(elems)
+for (let elem of elems) {
+    elem.addEventListener('mousemove', function(e) {
+        console.log('x = ' + e.screenX + ' Y = ' + e.screenY)
+    })
+    elem.addEventListener('click', function(e) {
+        alert(elem.innerHTML)
+    })
+}
