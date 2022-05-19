415fc2fca74764ce0716d3c6347695b39ff32bc4
B-MNSN <manutsanan0@gmail.com>
2022-05-19 12:08:02 +0700

    showText

--- script.js
+++ script.js
@@ -18,16 +18,45 @@
 //         alert(elem.innerHTML)
 //     })
 // }
-document.getElementById('ok').addEventListener('click', function(e) {
-    let noElement = document.getElementById('no')
-    if (noElement.classList.contains('toggleOn')) {
-        noElement.classList.replace('toggleOn', 'toggleOff')
-    } else if (noElement.classList.contains('toggleOff')) {
-        noElement.classList.replace('toggleOff', 'toggleOn')
-    } else(
-        noElement.classList.add('toggleOn')
-    )
+// document.getElementById('ok').addEventListener('click', function(e) {
+//     let noElement = document.getElementById('no')
+//     if (noElement.classList.contains('toggleOn')) {
+//         noElement.classList.replace('toggleOn', 'toggleOff')
+//     } else if (noElement.classList.contains('toggleOff')) {
+//         noElement.classList.replace('toggleOff', 'toggleOn')
+//     } else(
+//         noElement.classList.add('toggleOn')
+//     )
+// })
+
+// var borderTick = 2;
+// document.getElementById('cancel').style.border = `${borderTick}px solid white`;
+
+let margin = 20
+document.getElementById("ok").addEventListener('click', function(e) {
+    margin += 5
+    let marginSize = `20px ${margin}px`
+    document.getElementById('cancel').style.margin = marginSize
+    document.getElementById('no').style.margin = marginSize
+    margin++
 })
 
-var borderTick = 2;
-document.getElementById('cancel').style.border = `${borderTick}px solid white`;
+let showText = ['ok', 'วิชานี้', 'ง่าย', 'จริงๆนะ']
+let showIndex = 0
+document.getElementById('no').addEventListener('dblclick', function(e) {
+    showIndex++
+    if (showIndex >= showText.length) {
+        showIndex = 0;
+    }
+    const okButton = document.getElementById('ok')
+    okButton.innerText = showText[showIndex]
+        // if (okButton.innerText === 'ok') {
+        //     okButton.innerText = 'วิชานี้'
+        // } else if (okButton.innerText === 'วิชานี้') {
+        //     okButton.innerText = 'ง่าย'
+        // } else if (okButton.innerText === 'ง่าย') {
+        //     okButton.innerText = 'จริงๆนะ'
+        // } else if (okButton.innerText === 'จริงๆนะ') {
+        //     okButton.innerText = 'ok'
+        // }
+})
