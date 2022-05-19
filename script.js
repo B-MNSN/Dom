fd8a3ab426e3d53194fb0c97794b6027435aeca3
B-MNSN <manutsanan0@gmail.com>
2022-05-19 11:21:05 +0700

    toggle

--- script.js
+++ script.js
@@ -8,13 +8,26 @@
 //     alert('no is clicked')
 // })
 
-let elems = document.getElementsByClassName('flex-item')
-console.log(elems)
-for (let elem of elems) {
-    elem.addEventListener('mousemove', function(e) {
-        console.log('x = ' + e.screenX + ' Y = ' + e.screenY)
-    })
-    elem.addEventListener('click', function(e) {
-        alert(elem.innerHTML)
-    })
-}
+// let elems = document.getElementsByClassName('flex-item')
+// console.log(elems)
+// for (let elem of elems) {
+//     elem.addEventListener('mousemove', function(e) {
+//         console.log('x = ' + e.screenX + ' Y = ' + e.screenY)
+//     })
+//     elem.addEventListener('click', function(e) {
+//         alert(elem.innerHTML)
+//     })
+// }
+document.getElementById('ok').addEventListener('click', function(e) {
+    let noElement = document.getElementById('no')
+    if (noElement.classList.contains('toggleOn')) {
+        noElement.classList.replace('toggleOn', 'toggleOff')
+    } else if (noElement.classList.contains('toggleOff')) {
+        noElement.classList.replace('toggleOff', 'toggleOn')
+    } else(
+        noElement.classList.add('toggleOn')
+    )
+})
+
+var borderTick = 2;
+document.getElementById('cancel').style.border = `${borderTick}px solid white`;
